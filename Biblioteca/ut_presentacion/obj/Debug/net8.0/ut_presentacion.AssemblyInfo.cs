5608ace5670c9d8f56377710bcefc65acddf8def
Diego-Alejo <bstivenacetobon888@gmail.com>
2025-10-30 11:06:15 -0500

    Controllers

--- Biblioteca/ut_presentacion/obj/Debug/net8.0/ut_presentacion.AssemblyInfo.cs
+++ Biblioteca/ut_presentacion/obj/Debug/net8.0/ut_presentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ut_presentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0668cbf28806a50cc05c0b45db63374af7d38bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48bd0172d4de8b76fd34d3920a7735d9e81699ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("ut_presentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ut_presentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
